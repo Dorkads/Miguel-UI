a6fd7ff94c475c54bab1ee5a0ea904f1bd20b8bc
Ruslan <homastavi@gmail.com>
2024-05-08 17:11:17 +0300

    added animation for burger button

--- JS/JSmodules/modal.js
+++ JS/JSmodules/modal.js
@@ -1,10 +1,20 @@
 function modal() {
-  const modalBtn = document.querySelector("#modalBtn");
+  // находим кнопку и элемент навигации
+  const modalBtn  = document.querySelector("#modalBtn");
   const mobileNav = document.querySelector("#mobileNav");
+
+  // находим линии бургера
+  const firstLine   = document.querySelector('.burger__item-1');
+  const secondLine  = document.querySelector('.burger__item-2');
+  const threeLine   = document.querySelector('.burger__item-3')
 
   modalBtn.addEventListener("click", () => {
     mobileNav.style.display =
-      mobileNav.style.display === "none" ? "block" : "none";
+      mobileNav.style.display === "block" ? "none" : "block";
+
+    firstLine.classList.toggle('firstLine');
+    secondLine.classList.toggle('secondLine');
+    threeLine.classList.toggle('threeLine');
   });
 }
 
